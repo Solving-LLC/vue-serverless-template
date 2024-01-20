6ba2b74cd3263d1938fd62dfc7b28c739800ffe8
Vladimir V.M <vvmyagdeev@gmail.com>
2024-01-20 11:40:36 +0400

    feat<prettier>: code format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,12 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
-    'plugin:node/recommended',
+    'plugin:node/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
